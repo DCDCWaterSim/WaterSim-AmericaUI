71de4df12c43011e98521fb02dcbbf7922f72ac6
Dr. David Arthur Sampson <david.a.sampson@asu.edu>
2017-11-30 14:38:18 -0700

    Initial commit

---
+++ WaterSimUI/wwwroot/Scripts/Custom/Ipad/STC/STC.js
@@ -0,0 +1,17 @@
+var STC = {
+    "Florida": { "IFLDS": ["GWM", "SWM", "RECM", "UCON", "PCON", "AGCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND", "UEF"], "IDFLDS": [0] },
+    "Idaho": { "IFLDS": ["GWM", "SWM", "AGCON", "UCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [1] },
+    "Illinois": { "IFLDS": ["GWM", "SWM", "RECM", "UCON", "PCON", "AGCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "PEF", "UEF"], "IDFLDS": [2] },
+    "Minnesota": { "IFLDS": ["GWM", "SWM", "ICON", "UCON", "PCON","AGCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "PEF", "UEF"], "IDFLDS": [3] },
+    "Wyoming": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [4] },
+    "Arizona": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [5] },
+    "Colorado": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [6] },
+    "Nevada": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [7] },
+    "California": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [8] },
+    "Utah": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [9] },
+    "New Mexico": { "IFLDS": ["GWM", "SWM", "AGCON","UCON","ICON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [10] },
+    "Alabama": { "IFLDS": ["GWM", "SWM", "UCON", "ICON", "PCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "UEF"], "IDFLDS": [11] },
+    "Kansas": { "IFLDS": ["GWM", "SWM", "AGCON","UCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "AGIND"], "IDFLDS": [12] },
+    "Tennessee": { "IFLDS": ["GWM", "SWM", "UCON", "ICON", "PCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "PEF"], "IDFLDS": [13] },
+    "Virginia": { "IFLDS": ["GWM", "SWM", "UCON", "PCON"], "INDFLDS": ["GWSYA", "ECOR", "SWI", "PEF"], "IDFLDS": [14] }
+}
